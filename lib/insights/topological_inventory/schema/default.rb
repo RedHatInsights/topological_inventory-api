fe21de10ef41dfc6ee4fdf9c930827fd25bbcccd
Adam Grare <agrare@redhat.com>
2018-09-28 13:47:11 -0400

    Fix default persister

--- lib/insights/topological_inventory/schema/default.rb
+++ lib/insights/topological_inventory/schema/default.rb
@@ -6,12 +6,12 @@ module Insights
       class Default < InventoryRefresh::Persister
         def initialize_inventory_collections
           %i(container_projects container_groups container_templates
-             service_offering service_instances service_parameters_sets).each do |model|
+             service_offerings service_instances service_parameters_sets).each do |model|
                add_collection(model) do |builder|
                  builder.add_properties(
                    :manager_ref => %i(source_ref),
                  )
-                 builder.add_default_values(:ems_id => ->(persister) { persister.manager.id })
+                 builder.add_default_values(:source_id => ->(persister) { persister.manager.id })
                end
              end
         end
