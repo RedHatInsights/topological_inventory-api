705e9e593fd09b54d637bff467cf9a59b3dec5cc
Brandon Dunne <brandondunne@hotmail.com>
2018-10-04 15:11:14 -0400

    Switch to a more reasonable base path

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,9 @@ Rails.application.routes.draw do
     match full_path, to: "#{opts.fetch(:controller_name)}##{opts[:action_name]}", via: http_method
   end
 
-  add_swagger_route 'GET', '/Fryguy/topological_inventory/0.0.1/providers', controller_name: 'users', action_name: 'list_providers'
-  add_swagger_route 'GET', '/Fryguy/topological_inventory/0.0.1/service_offerings', controller_name: 'users', action_name: 'list_service_offerings'
-  add_swagger_route 'GET', '/Fryguy/topological_inventory/0.0.1/service_parameters_sets', controller_name: 'users', action_name: 'list_service_parameters_sets'
+  add_swagger_route 'GET', '/0.0.1/providers', controller_name: 'users', action_name: 'list_providers'
+  add_swagger_route 'GET', '/0.0.1/service_offerings', controller_name: 'users', action_name: 'list_service_offerings'
+  add_swagger_route 'GET', '/0.0.1/service_parameters_sets', controller_name: 'users', action_name: 'list_service_parameters_sets'
   scope "api" do
     scope "0.0.1" do
       resources :sources, only: [:create, :destroy, :index, :show, :update]
