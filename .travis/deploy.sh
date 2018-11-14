82980747f9ace5ab86947c37295fb828d06d4e13
Jason Frey <fryguy9@gmail.com>
2018-11-14 15:34:54 -0500

    Add deployment feedback

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -e
+
 TAG="$DEPLOY_SERVER/$DEPLOY_NAMESPACE/$DEPLOY_IMAGE:latest"
 
+echo -e "\033[1;36mLogging into registry...\033[0m"
 echo $DEPLOY_TOKEN | docker login -u $DEPLOY_USER --password-stdin $DEPLOY_SERVER
+
+echo -e "\033[1;36mBuilding docker image...\033[0m"
 docker build -t $TAG .
+
+echo -e "\033[1;36mPushing docker image...\033[0m"
 docker push $TAG
