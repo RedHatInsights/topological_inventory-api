9e2381cea6ef606856c2c38898781a9cd77d4ff2
Madhu Kanoor <mkanoor@redhat.com>
2018-12-11 17:06:30 -0500

    Added OrderParameters to the ignore list

--- spec/swagger_spec.rb
+++ spec/swagger_spec.rb
@@ -87,7 +87,7 @@ describe "Swagger stuff" do
     context "v0.0" do
       let(:version) { "0.0" }
       Api::Docs["0.0"].definitions.each do |definition_name, schema|
-        next if definition_name.in?(["Id"])
+        next if definition_name.in?(["Id", "OrderParameters"])
 
         it "#{definition_name} matches the JSONSchema" do
           const = definition_name.constantize
