b87e1783cdffc02228784a4888164f02d82735eb
Brandon Dunne <brandondunne@hotmail.com>
2018-12-19 12:06:42 -0500

    Id is no longer a definition

--- spec/swagger_spec.rb
+++ spec/swagger_spec.rb
@@ -101,7 +101,7 @@ describe "Swagger stuff" do
     context "v0.0" do
       let(:version) { "0.0" }
       Api::Docs["0.0"].definitions.each do |definition_name, schema|
-        next if definition_name.in?(["Id", "OrderParameters"])
+        next if definition_name.in?(["OrderParameters"])
 
         it "#{definition_name} matches the JSONSchema" do
           const = definition_name.constantize
