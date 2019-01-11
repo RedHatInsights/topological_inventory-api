f660b9656501508a94231a2f94b2d358181b1340
Ladislav Smola <lsmola@redhat.com>
2019-01-11 09:29:18 +0100

    Automatically preload relations for taggables to avoid n+1

--- app/controllers/api/v0/container_images_controller.rb
+++ app/controllers/api/v0/container_images_controller.rb
@@ -4,10 +4,6 @@ module Api
       include Api::V0::Mixins::IndexMixin
       include Api::V0::Mixins::ShowMixin
 
-      def index
-        render json: model.where(list_params).includes(:container_image_tags => :tag).references(:container_image_tags => :tag)
-      end
-
       private
 
       def list_params
