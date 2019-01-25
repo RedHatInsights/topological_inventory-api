b233e581bc55eece661afe7506983d67f92d21a5
Ladislav Smola <lsmola@redhat.com>
2019-01-25 11:43:56 +0100

    Fix includes because of moved mixins

--- app/controllers/api/v0/tags_controller.rb
+++ app/controllers/api/v0/tags_controller.rb
@@ -1,8 +1,8 @@
 module Api
   module V0
     class TagsController < ApplicationController
-      include Api::Mixins::IndexMixin
-      include Api::Mixins::ShowMixin
+      include Api::V0::Mixins::IndexMixin
+      include Api::V0::Mixins::ShowMixin
 
       def list_params
         params.permit(:tenant_id)
