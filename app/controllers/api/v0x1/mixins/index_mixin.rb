794f5c59a431e72bb4358dec5dc6d4dd103d7a63
Ladislav Smola <lsmola@redhat.com>
2019-01-25 13:05:34 +0100

    Remove n+1 tagging requests

--- app/controllers/api/v0x1/mixins/index_mixin.rb
+++ app/controllers/api/v0x1/mixins/index_mixin.rb
@@ -4,11 +4,21 @@ module Api
       module IndexMixin
         def index
           render json: ManageIQ::API::Common::PaginatedResponse.new(
-            base_query: model.where(list_params),
+            base_query: scoped(model.where(list_params)),
             request: request,
             limit: params.permit(:limit)[:limit],
             offset: params.permit(:offset)[:offset]
           ).response
+        end
+
+        def scoped(relation)
+          if model.respond_to?(:taggable?) && model.taggable?
+            ref_schema = {model.tagging_relation_name => :tag}
+
+            relation = relation.includes(ref_schema).references(ref_schema)
+          end
+
+          relation
         end
       end
     end
