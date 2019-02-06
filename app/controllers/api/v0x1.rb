91834d2c7bb32f20374b13271cdfadf602aec6fe
Brandon Dunne <brandondunne@hotmail.com>
2019-02-06 14:50:11 -0500

    Add /v*/openapi.json to fetch the swagger yaml

--- app/controllers/api/v0x1.rb
+++ app/controllers/api/v0x1.rb
@@ -1,5 +1,11 @@
 module Api
   module V0x1
+    class RootController < ApplicationController
+      def openapi
+        render :json => Api::Docs["0.1"].to_json
+      end
+    end
+
     class AuthenticationsController < Api::V0x0::AuthenticationsController
       include Api::V0x1::Mixins::CreateMixin
       include Api::V0x1::Mixins::DestroyMixin
