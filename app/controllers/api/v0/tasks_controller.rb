934fdc0f41e011bdad641f4a781ef0ce20d00de5
Erik Clarizio <eclarizi@redhat.com>
2019-02-25 10:58:39 -0800

    Add v0x1 tasks_controller_spec

--- app/controllers/api/v0/tasks_controller.rb
+++ app/controllers/api/v0/tasks_controller.rb
@@ -4,20 +4,6 @@ module Api
       include Api::V0::Mixins::IndexMixin
       include Api::V0::Mixins::ShowMixin
       include Api::V0::Mixins::UpdateMixin
-
-      private
-
-      def list_params
-        params.permit(:tenant_id)
-      end
-
-      def update_params
-        params.permit(:status, :context)
-      end
-
-      def model
-        Task
-      end
     end
   end
 end
--- spec/controllers/api/v0x1/tasks_controller_spec.rb
+++ spec/controllers/api/v0x1/tasks_controller_spec.rb
@@ -1,5 +1,5 @@
-RSpec.describe Api::V0x0::TasksController, :type => :request do
-  it("Uses IndexMixin")   { expect(described_class.instance_method(:index).owner).to eq(Api::V0::Mixins::IndexMixin) }
+RSpec.describe Api::V0x1::TasksController, :type => :request do
+  it("Uses IndexMixin")   { expect(described_class.instance_method(:index).owner).to eq(Api::V0x1::Mixins::IndexMixin) }
   it("Uses ShowMixin")    { expect(described_class.instance_method(:show).owner).to eq(Api::V0::Mixins::ShowMixin) }
   it("Uses UpdateMixin")  { expect(described_class.instance_method(:update).owner).to eq(Api::V0::Mixins::UpdateMixin) }
 
@@ -8,7 +8,7 @@ RSpec.describe Api::V0x0::TasksController, :type => :request do
   it "patch /tasks/:id updates a Task" do
     task = Task.create!(:status => "started", :context => "context1", :tenant => tenant)
 
-    patch(api_v0x0_task_url(task.id), :params => {:status => "completed", :context => "context2"}.to_json)
+    patch(api_v0x1_task_url(task.id), :params => {:status => "completed", :context => "context2"}.to_json)
 
     expect(task.reload.status).to eq("completed")
     expect(task.context).to eq("context2")
