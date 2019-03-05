c77d361ab0a309ce1efbefb5eaa375a07076d371
Adam Grare <agrare@redhat.com>
2019-03-05 13:56:58 -0500

    Tasks need to be updatable through the API
    The openshift-operations worker is updating tasks through the API, so we
    need to include the update mixin in the tasks controller.

--- app/controllers/api/v0/tasks_controller.rb
+++ app/controllers/api/v0/tasks_controller.rb
@@ -3,6 +3,7 @@ module Api
     class TasksController < ApplicationController
       include Api::V0::Mixins::IndexMixin
       include Api::V0::Mixins::ShowMixin
+      include Api::V0::Mixins::UpdateMixin
     end
   end
 end
