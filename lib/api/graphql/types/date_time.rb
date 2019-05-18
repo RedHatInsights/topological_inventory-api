3e2802103573531d88fbef1d2929dce5b7e29fe8
Alberto Bellotti <abellott@redhat.com>
2019-05-17 23:58:53 -0400

    Add support for DateTime and BigInt types

---
+++ lib/api/graphql/types/date_time.rb
@@ -0,0 +1,13 @@
+module Api
+  module GraphQL
+    module Types
+      DateTime = ::GraphQL::ScalarType.define do
+        name "DateTime"
+        description "An ISO-8601 encoded UTC date string."
+
+        # coerce_input ->(value, _ctx) { Time.zone.parse(value) }
+        # coerce_result ->(value, _ctx) { value.utc.iso8601 }
+      end
+    end
+  end
+end
