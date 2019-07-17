ff1230cb62ca6cfc5b5be99a9b267d6054334029
Adam Grare <agrare@redhat.com>
2019-07-17 12:47:17 -0400

    Comment the CfmeManifst.find method

--- lib/topological_inventory/api/cfme_manifest.rb
+++ lib/topological_inventory/api/cfme_manifest.rb
@@ -4,7 +4,11 @@ module TopologicalInventory
       def self.find(version)
         version.gsub!(".", "_")
 
-        manifest = nil
+        # Find the manifest file that best matches the version supplied.
+        # Look for a full match first then iteratively check for broader
+        # and broader versions.
+        #
+        # E.g. 5.11.0.0 will check for 5.11.0.0, then 5.11.0, 5.11, then 5
         until version.blank? || (manifest = manifest_by_version[version]).present?
           version = version.split("_")[0...-1].join("_")
         end
@@ -13,9 +17,11 @@ module TopologicalInventory
       end
 
       private_class_method def self.manifest_by_version
-        @manifest_by_version ||= Pathname.glob(Rails.root.join("config", "cfme", "manifest_*.json")).index_by do |file|
-          file.basename.to_s.match(/manifest_(.*)\.json/)[1]
-        end
+        # List all manifests and cache the result to prevent a readdir on every API call
+        @manifest_by_version ||=
+          Pathname.glob(Rails.root.join("config", "cfme", "manifest_*.json")).index_by do |file|
+            file.basename.to_s.match(/manifest_(.*)\.json/)[1]
+          end
       end
     end
   end
