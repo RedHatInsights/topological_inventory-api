538010c3753f69f0e235ae298a966e8cd96193d5
Brandon Dunne <brandondunne@hotmail.com>
2019-09-20 16:33:09 -0400

    Fix error expectations on invalid ids

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -1,5 +1,7 @@
 module Api
   class RootController < ApplicationController
+    skip_before_action :validate_primary_collection_id
+
     def invalid_url_error
       error_document = ManageIQ::API::Common::ErrorDocument.new.add(404, "Invalid URL /#{params[:path]} specified.")
       render :json => error_document.to_h, :status => :not_found
